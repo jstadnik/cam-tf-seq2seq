056854635cc714f4ef5c9df741743a0e7b501ba2
DCSaunders <ds636@cam.ac.uk>
2017-09-01 13:45:56 +0100

    fix path to multi-bleu script

--- cam_tf_new/train.py
+++ cam_tf_new/train.py
@@ -61,7 +61,7 @@ tf.app.flags.DEFINE_integer("eval_size", 80, "The number of examples to evaluate
 tf.app.flags.DEFINE_boolean("eval_bleu", False, "If True, decode dev set and measure BLEU instead of measuring perplexities")
 tf.app.flags.DEFINE_integer("eval_bleu_size", 0, "The number of dev sentences to translate (all if set to 0)")
 tf.app.flags.DEFINE_integer("eval_bleu_start", 10000, "Number of batches before starting BLEU evaluation on dev")
-tf.app.flags.DEFINE_string("eval_cmd", "multi-bleu.perl REF -lc", "Path to BLEU eval script with space-delimited args. REF to be replaced by ref idx if needed. OUT to be replaced by out idx. If OUT is omitted, out idx is fed into stdin for the eval script")
+tf.app.flags.DEFINE_string("eval_cmd", "/home/mifs/ds636/code/scripts/multi-bleu.perl REF -lc", "Path to BLEU eval script with space-delimited args. REF to be replaced by ref idx if needed. OUT to be replaced by out idx. If OUT is omitted, out idx is fed into stdin for the eval script")
 
 # Model configuration
 tf.app.flags.DEFINE_integer("src_vocab_size", 40000, "Source vocabulary size.")
--- cam_tf_original/utils/train_utils.py
+++ cam_tf_original/utils/train_utils.py
@@ -258,8 +258,7 @@ def eval_set(out, ref):
   import subprocess
   cat = subprocess.Popen(("cat", out), stdout=subprocess.PIPE)
   try:
-    multibleu = subprocess.check_output(("multi-bleu.perl", "-lc",
-                                       ref), stdin=cat.stdout)
+    multibleu = subprocess.check_output(("/home/mifs/ds636/code/scripts/multi-bleu.perl", "-lc", ref), stdin=cat.stdout)
     logging.info("{}".format(multibleu))
     import re
     m = re.match("BLEU = ([\d.]+),", multibleu)
